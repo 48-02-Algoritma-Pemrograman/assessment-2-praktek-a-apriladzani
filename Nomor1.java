394068b361eb70837de483a2ff36bf2c93fee4db
apriladzani <april.adzania@gmail.com>
2024-12-12 11:56:46 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -14,15 +14,18 @@ public class Nomor1 {
     }
 
     public static void hitung(int n){
-        for (int i = 0; i <= n; i++) {
-            if (i % 3 == 0) {
+         for (int i = 0; i <= n; i++) {
+            if (i==0) {
+                System.out.print("0 ");
+            } else if (i % 3 == 0 ) {
                 System.out.print("Fizz ");
             } else if (i % 5 == 0) {
                 System.out.print("Buzz ");
             } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FizzBuzz ");
+            } else {
+                System.out.print(i + " ");
             } 
         }
-    }
-    
+    }  
 }
