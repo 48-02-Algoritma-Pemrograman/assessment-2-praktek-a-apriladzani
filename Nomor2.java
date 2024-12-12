1b9cd17020ded987a5d57a477a5e950a7131a0d2
apriladzani <april.adzania@gmail.com>
2024-12-12 12:46:32 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -7,6 +7,18 @@ public class Nomor2 {
         /* Pekerjaan anda mulai dari sini */
         String[] daftarBuku = {"Sherlock Holmes", "Arsene Lupin", "Game of Throne", "Psychology of Money"};
         int[] hargaBuku = {350000, 450000, 500000, 95000};
+
+        boolean a = true;
+        while (a) {
+            System.out.print("Masukan pilihan buku [1/2/3/4]:");
+            int pilihan = scanner.nextInt();
+            hargaBelanja.add(hargaBuku[pilihan - 1]);
+            System.out.print("Input lagi (Y/N)?: ");
+            String jawaban = scanner.next();
+            if (jawaban.equalsIgnoreCase("N")) {
+                a = false;
+            }
+        }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor2.class.getDeclaredMethods().length); 
