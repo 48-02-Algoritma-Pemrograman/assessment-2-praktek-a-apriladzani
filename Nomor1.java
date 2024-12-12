de7739d96ce13ded6f81e1140b49c83c28021058
apriladzani <april.adzania@gmail.com>
2024-12-12 11:41:39 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -7,9 +7,24 @@ public class Nomor1 {
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
         scanner.close();
+        hitung(n);
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
+    }
+
+    public static void hitung(int n){
+        for (int i = 0; i <= n; i++) {
+            if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
     }
     
 }
